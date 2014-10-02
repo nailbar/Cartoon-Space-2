44b04ebfded1cc8ceba269cd34454a2f68b1dfc2
Martin Wikström <martin@katrina.fi>
2014-10-02 12:16:34 +0300

    Added collision avoidance and base for friendly fire avoidance to AI

--- default/ai.class.js
+++ default/ai.class.js
@@ -11,8 +11,66 @@ class_ai.prototype.think = function(ship, ship_id, ships, speed) {
     var tmp = {
         'turn': 0.0,
         'thrust': 0.0,
-        'fire': 0
+        'fire': 0,
+        'friendlyfire': 0,
+        'collision': 0
     };
+    
+    // Add random ship to collision detection
+    tmp.id = Math.floor(Math.random() * ships.length);
+    tmp.addthis = true;
+    if(tmp.id == ship_id) tmp.addthis = false;
+    for(var i = 0; i < this.nearest.length; i++) if(this.nearest[i] == tmp.id) tmp.addthis = false;
+    if(tmp.addthis) this.nearest.push(tmp.id);
+    
+    // Perform collision detection on nearby ships
+    tmp.removeme = 0;
+    tmp.removedistance = 0;
+    for(var i = 0; i < this.nearest.length; i++) if(this.nearest[i] < 0 || this.nearest[i] >= ships.length || this.nearest[i] == ship_id) {
+        
+        // Remove unusable id
+        this.nearest.splice(i, 1);
+        i--;
+    } else {
+        tmp.data = this.calculate(ship, ships[this.nearest[i]].position);
+        
+        // Remove those behind you
+        if(tmp.data.dot < 0 && tmp.data.distance > (ship.size + ships[this.nearest[i]].size) * 1.5) {
+            this.nearest.splice(i, 1);
+            i--;
+        } else {
+            
+            // Flag furthest away for removal
+            if(tmp.data.distance > tmp.removedistance || tmp.data.dot < 0) {
+                tmp.removeme = i;
+                tmp.removedistance = tmp.data.distance;
+            }
+            
+            // Avoid friendly fire
+            //  * TODO: Irrelevant until we have actual teams
+            if(false) if(tmp.data.distance < (ship.size + ships[this.nearest[i]].size) * 1.5 || (tmp.data.dot > 0.8 && tmp.data.distance < 3000.0)) tmp.friendlyfire = true;
+            
+            // Avoid collision
+            if(tmp.data.distance < (ship.size + ships[this.nearest[i]].size) * 2) {
+                tmp.collision = true;
+                if(tmp.data.dot < 0.4) {
+                    tmp.thrust = 1.0;
+                    if(tmp.data.dot_right < -0.4) tmp.turn = -1.0;
+                    else if(tmp.data.dot_right > 0.4) tmp.turn = 1.0;
+                } else {
+                    if(tmp.data.dot_right < 0.0) tmp.turn = -1.0;
+                    else tmp.turn = 1.0;
+                }
+            }
+        }
+    }
+    
+// // DEBUG collision avoidance
+// tmp.collision = true;
+// tmp.friendlyfire = true;
+    
+    // Remove furthest away if list is full
+    if(this.nearest.length > 5) this.nearest.splice(tmp.removeme, 1);
     
     // Add some random into it
     this.ticker -= speed;
@@ -41,14 +99,16 @@ class_ai.prototype.think = function(ship, ship_id, ships, speed) {
         tmp.data = this.calculate(ship, ships[this.target].position);
         if(tmp.data.distance < 500.0) this.state = 2;
         else {
-            if(tmp.data.dot > -0.8) {
-                if(tmp.data.distance > 600.0) tmp.thrust = 1.0;
-                if(tmp.data.dot_right < -0.4) tmp.turn = 1.0;
-                else if(tmp.data.dot_right > 0.4) tmp.turn = -1.0;
-                else tmp.turn = tmp.data.dot_right * -2.0;
-            } else {
-                if(tmp.data.dot_right < 0.0) tmp.turn = 1.0;
-                else tmp.turn = -1.0;
+            if(!tmp.collision) {
+                if(tmp.data.dot > -0.8) {
+                    if(tmp.data.distance > 600.0) tmp.thrust = 1.0;
+                    if(tmp.data.dot_right < -0.4) tmp.turn = 1.0;
+                    else if(tmp.data.dot_right > 0.4) tmp.turn = -1.0;
+                    else tmp.turn = tmp.data.dot_right * -2.0;
+                } else {
+                    if(tmp.data.dot_right < 0.0) tmp.turn = 1.0;
+                    else tmp.turn = -1.0;
+                }
             }
             if(tmp.data.dot > 0.95 && tmp.data.distance < 2000.0) tmp.fire = 1;
         }
@@ -59,13 +119,15 @@ class_ai.prototype.think = function(ship, ship_id, ships, speed) {
         tmp.data = this.calculate(ship, ships[this.target].position);
         if(tmp.data.distance > 2500.0) this.state = 1;
         else {
-            if(tmp.data.dot < 0.4) {
-                tmp.thrust = 1.0;
-                if(tmp.data.dot_right < -0.4) tmp.turn = -1.0;
-                else if(tmp.data.dot_right > 0.4) tmp.turn = 1.0;
-            } else {
-                if(tmp.data.dot_right < 0.0) tmp.turn = -1.0;
-                else tmp.turn = 1.0;
+            if(!tmp.collision) {
+                if(tmp.data.dot < 0.4) {
+                    tmp.thrust = 1.0;
+                    if(tmp.data.dot_right < -0.4) tmp.turn = -1.0;
+                    else if(tmp.data.dot_right > 0.4) tmp.turn = 1.0;
+                } else {
+                    if(tmp.data.dot_right < 0.0) tmp.turn = -1.0;
+                    else tmp.turn = 1.0;
+                }
             }
         }
         break;
@@ -73,14 +135,16 @@ class_ai.prototype.think = function(ship, ship_id, ships, speed) {
     // Berserk state (an attack state which doesn't change by distance)
     case 3:
         tmp.data = this.calculate(ship, ships[this.target].position);
-        if(tmp.data.dot > -0.8) {
-            if(tmp.data.distance > 600.0) tmp.thrust = 1.0;
-            if(tmp.data.dot_right < -0.4) tmp.turn = 1.0;
-            else if(tmp.data.dot_right > 0.4) tmp.turn = -1.0;
-            else tmp.turn = tmp.data.dot_right * -2.0;
-        } else {
-            if(tmp.data.dot_right < 0.0) tmp.turn = 1.0;
-            else tmp.turn = -1.0;
+        if(!tmp.collision) {
+            if(tmp.data.dot > -0.8) {
+                if(tmp.data.distance > 600.0) tmp.thrust = 1.0;
+                if(tmp.data.dot_right < -0.4) tmp.turn = 1.0;
+                else if(tmp.data.dot_right > 0.4) tmp.turn = -1.0;
+                else tmp.turn = tmp.data.dot_right * -2.0;
+            } else {
+                if(tmp.data.dot_right < 0.0) tmp.turn = 1.0;
+                else tmp.turn = -1.0;
+            }
         }
         if(tmp.data.dot > 0.95 && tmp.data.distance < 2000.0) tmp.fire = 1;
         break;
@@ -90,6 +154,7 @@ class_ai.prototype.think = function(ship, ship_id, ships, speed) {
     ship.thrust += (tmp.thrust - ship.thrust) * 0.5 * speed;
     ship.rotspeed += (tmp.turn * 0.1 - ship.rotspeed) * 0.1 * speed;
     ship.fireprimary = tmp.fire;
+    if(tmp.friendlyfire) ship.fireprimary = 0;
 }
 
 // Calculate relative target data
